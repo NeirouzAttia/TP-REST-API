4aba694557e59f36dc362b01721ec03682d72742
NeirouzAttia <neirouz.attia@etud.univ-jfc.fr>
2025-02-21 14:42:33 +0100

    Create ParticipationView.vue

---
+++ frontend/src/views/ParticipationView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ParticipationForm from "@/components/ParticipationForm.vue";
+</script>
+
+<template>
+  <div class="participation">
+    <ParticipationForm />
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .participation {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
